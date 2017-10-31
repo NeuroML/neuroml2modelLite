02a74f5bc8ec7d85b018c8fcf61165b6b1faf142
Padraig Gleeson <p.gleeson@gmail.com>
2017-10-31 18:54:31 +0000

    More ion chans supported

--- src/main/java/org/neuroml2/modellite/NeuroML2ModelReader.java
+++ src/main/java/org/neuroml2/modellite/NeuroML2ModelReader.java
@@ -88,13 +88,19 @@ public class NeuroML2ModelReader
                     }
                     else
                     {
-                        String expr = val;
+                        String expr = val.replaceAll(":", " : ").replaceAll("\\?", " ? ");
                         String ex = pre[2];
 
                         String title = pre[0] + " - " + pre[1] + " - " + pre[2];
 
                         double dv = 0.0025;
-                        l3.put(ex, new ExpressionNode(expr, title, "V", "ms-1", -0.1, 0.1 + dv, dv));
+                        String yaxis = "rate (1/s)";
+                        if (pre[2].contains("time"))
+                            yaxis = "tau (s)";
+                        if (pre[2].contains("steady"))
+                            yaxis = "steady state";
+                        ExpressionNode en = new ExpressionNode(expr, title, "Membrane potential (V)", yaxis, -0.1, 0.1 + dv, dv);
+                        l3.put(ex, en);
 
                     }
                 }
@@ -121,6 +127,8 @@ public class NeuroML2ModelReader
         {
             if (!(sae.getClass().getName().equals("org.neuroml.model.Network")
                 || sae.getClass().getName().equals("org.neuroml.model.Cell")
+                || sae.getClass().getName().equals("org.neuroml.model.Cell2CaPools")
+                || sae.getClass().getName().equals("org.neuroml.model.IonChannelKS")
                 || sae.getClass().getName().equals("org.neuroml.model.PoissonFiringSynapse")))
             {
                 System.out.println("-- Including " + sae.getClass().getName());
@@ -179,6 +187,7 @@ public class NeuroML2ModelReader
         //ionChannelFile = "../../geppetto/org.geppetto.model.neuroml/src/test/resources/hhcell/NML2_SingleCompHHCell.nml";
         //ionChannelFile = "/tmp/NeuroML_3644588102506634724.tmp";
         //ionChannelFile = "../../neuroConstruct/osb/cerebral_cortex/networks/Thalamocortical/NeuroML2/pythonScripts/netbuild/TestL23.net.nml";
+        ionChannelFile = "../../neuroConstruct/osb/cerebellum/networks/VervaekeEtAl-GolgiCellNetwork/NeuroML2/Golgi_040408_C1.net.nml";
         if (args.length == 1)
         {
             ionChannelFile = args[0];
